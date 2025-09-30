d4243747e8b54e0d197c1f14c5e6ca84f82b1325
Nathanael Dereje <151948534+nathanaeldereje@users.noreply.github.com>
2025-09-30 14:36:15 +0000

    [LeetCode Sync] Runtime - 4664 ms (5.00%), Memory - 0.0B (100.00%)

---
+++ solutions/1182-game-play-analysis-iv/solution.sql
@@ -0,0 +1,21 @@
+WITH first_login AS (
+    SELECT 
+        player_id,
+        MIN(event_date) AS first_date
+    FROM Activity
+    GROUP BY player_id
+),
+next_day_login AS (
+    SELECT 
+        f.player_id
+    FROM first_login f
+    JOIN Activity a
+      ON f.player_id = a.player_id
+     AND a.event_date = DATE_ADD(f.first_date, INTERVAL 1 DAY)
+)
+SELECT 
+    ROUND(COUNT(DISTINCT n.player_id) / CAST(COUNT(DISTINCT a.player_id) AS DECIMAL), 2) AS fraction
+FROM Activity a
+LEFT JOIN next_day_login n
+  ON a.player_id = n.player_id;
+
