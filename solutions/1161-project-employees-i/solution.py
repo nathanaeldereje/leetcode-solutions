a6e5b167cf16343e8cdf1beaebcb63e7e90c299a
Nathanael Dereje <151948534+nathanaeldereje@users.noreply.github.com>
2025-10-27 14:58:41 +0000

    [LeetCode Sync] Runtime - 297 ms (34.38%), Memory - 68.6 MB (84.51%)

---
+++ solutions/1161-project-employees-i/solution.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def project_employees_i(project: pd.DataFrame, employee: pd.DataFrame) -> pd.DataFrame:
+
+    merged = project.merge(employee, on='employee_id')
+    
+    result = (
+        merged.groupby('project_id', as_index=False)['experience_years']
+        .mean()
+        .round(2)
+        .rename(columns={'experience_years': 'average_years'})
+    )
+    
+    return result
+
