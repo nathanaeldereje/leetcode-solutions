8045f8bbfdb94edc0924d1d71894a03f1a7a9990
Nathanael Dereje <151948534+nathanaeldereje@users.noreply.github.com>
2025-11-05 02:33:30 +0000

    [LeetCode Sync] Runtime - 246 ms (90.51%), Memory - 67 MB (95.58%)

---
+++ solutions/0620-not-boring-movies/solution.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def not_boring_movies(cinema: pd.DataFrame) -> pd.DataFrame:
+    selected=cinema[(cinema['id']%2!=0)&(cinema['description']!='boring')].sort_values(by='rating',ascending=False)
+    return selected
+    
