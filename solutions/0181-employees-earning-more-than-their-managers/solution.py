5828d6ba525fab2760e40197fd774e0eb981f687
Nathanael Dereje <151948534+nathanaeldereje@users.noreply.github.com>
2026-04-06 05:26:08 +0000

    [LeetCode Sync] Runtime - 274 ms (90.36%), Memory - 68.6 MB (29.51%)

--- solutions/0181-employees-earning-more-than-their-managers/solution.py
+++ solutions/0181-employees-earning-more-than-their-managers/solution.py
@@ -1,8 +1,6 @@
 import pandas as pd
 
 def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
-    merged = employee.merge(employee, how='inner', left_on='managerId', right_on='id', suffixes=('_emp', '_mgr'))
-    result = merged[merged['salary_emp'] > merged['salary_mgr']][['name_emp']]
-    result.columns = ['Employee']
-
-    return(result)
+    df=pd.merge(employee, employee, left_on='managerId', right_on='id',how='left')
+    return df[df['salary_x'] > df['salary_y']][['name_x']] \
+           .rename(columns={'name_x': 'Employee'})
