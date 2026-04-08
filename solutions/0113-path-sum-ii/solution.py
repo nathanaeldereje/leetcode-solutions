82c8c4f5348bfb7f28d38a792c1c2c1be2e69151
Nathanael Dereje <151948534+nathanaeldereje@users.noreply.github.com>
2026-04-08 06:27:31 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.1 MB (71.46%)

---
+++ solutions/0113-path-sum-ii/solution.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        all_nums=[]
+        nums=[]
+        def innerFunction(node, nums):
+            if not node:
+                return 
+            nums.append(node.val)
+            if not node.left and not node.right: 
+                if sum(nums)==targetSum:
+                    all_nums.append(nums[:])
+                # print(nums)
+            else:
+                innerFunction(node.left, nums)
+                innerFunction(node.right, nums)
+            nums.pop()
+        innerFunction(root,nums)
+        return all_nums
+                
