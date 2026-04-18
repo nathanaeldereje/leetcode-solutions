e51bb19255e9cb2b0a4f981b2f1c7d65fdaace32
Nathanael Dereje <151948534+nathanaeldereje@users.noreply.github.com>
2026-04-18 04:43:02 +0000

    [LeetCode Sync] Runtime - 275 ms (75.32%), Memory - 67.6 MB (90.97%)

--- solutions/1135-customers-who-bought-all-products/solution.py
+++ solutions/1135-customers-who-bought-all-products/solution.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def find_customers(customer: pd.DataFrame, product: pd.DataFrame) -> pd.DataFrame:
     total_products = len(product)
     counts = customer.groupby('customer_id')['product_key'].nunique()
