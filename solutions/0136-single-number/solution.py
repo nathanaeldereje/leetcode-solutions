e21c653e877d04f0f5a01f1b39f4d206078a6b6c
Nathanael Dereje <151948534+nathanaeldereje@users.noreply.github.com>
2026-08-23 10:33:18 +0000

    [LeetCode Sync] Runtime - 3 ms (60.64%), Memory - 21.1 MB (69.83%)

--- solutions/0136-single-number/solution.py
+++ solutions/0136-single-number/solution.py
@@ -1,9 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        single_num = 0
-        
-        # XOR all elements in the array
+        result=0
         for num in nums:
-            single_num ^= num
-            
-        return single_num
+            result^=num
+        return result
+        
